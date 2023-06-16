64656311a384c1a4b3857d0116aca8d7a79d5319
James Johnson <jamesjhnsn1995@gmail.com>
2023-06-16 01:05:05 -0700

    created cdk pipeline and finished app stack

--- lib/eb-appln-stack.ts
+++ lib/eb-appln-stack.ts
@@ -84,6 +84,15 @@ export class EBApplnStack extends cdk.Stack {
     },
   ];
 
+  // Create an Elastic Beanstalk environment to run the application
+  const elbEnv = new elasticbeanstalk.CfnEnvironment(this, 'Environment', {
+    environmentName: props?.envName ?? "MyWebAppEnvironment",
+    applicationName: app.applicationName || appName,
+    solutionStackName: '64bit Amazon Linux 2 v5.8.0 running Node.js 18',
+    optionSettings: optionSettingProperties,
+    versionLabel: appVersionProps.ref,
+  });
+
 
   }
 }
