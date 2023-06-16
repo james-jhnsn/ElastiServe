caf3bd19c679f58f3bdc2613b6358a68a5127b58
James Johnson <jamesjhnsn1995@gmail.com>
2023-06-16 00:54:50 -0700

    setting up beanstalk environment

--- lib/eb-appln-stack.ts
+++ lib/eb-appln-stack.ts
@@ -1,3 +1,6 @@
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as elasticbeanstalk from 'aws-cdk-lib/aws-elasticbeanstalk';
+import * as s3assets from 'aws-cdk-lib/aws-s3-assets';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // Add import statements here
@@ -15,6 +18,72 @@ export class EBApplnStack extends cdk.Stack {
        super(scope, id, props);
 
     // The code that defines your stack goes here
+
+    // construct an S3 asset Zip from directory up.
+    // This code uses the S3 Assets module and takes the folder of the web app located in the root of the CDK app, compresses into a zip file and uploads it to S3. Whenever we update the application source code and push to the GitHub repo, the file will automatically get updated in S3.
+    const webAppZipArchive = new s3assets.Asset(this, 'webAppZip', {
+      path: `${__dirname}/../src`,
+    });
+
+    // create an ElasticBeanStalk app.
+    const appName = 'MyWebApp';
+    const app = new elasticbeanstalk.CfnApplication(this, 'Application', {
+      applicationName: appName,
+    });
+
+    // Create an app version from the S3 asset defined earlier
+    const appVersionProps = new elasticbeanstalk.CfnApplicationVersion(this, 'AppVersion', {
+      applicationName: appName,
+      sourceBundle: {
+        s3Bucket: webAppZipArchive.s3BucketName,
+        s3Key: webAppZipArchive.s3ObjectKey,
+      },
+    });
+
+    // Make sure that Elastic Beanstalk app exists before creating an app version
+    appVersionProps.addDependency(app);
+
+    // Create role and instance profile
+    const myRole = new iam.Role(this, `${appName}-aws-elasticbeanstalk-ec2-role`, {
+      assumedBy: new iam.ServicePrincipal('ec2.amazonaws.com'),
+    });
+
+    const managedPolicy = iam.ManagedPolicy.fromAwsManagedPolicyName('AWSElasticBeanstalkWebTier')
+    myRole.addManagedPolicy(managedPolicy);
+
+    const myProfileName = `${appName}-InstanceProfile`
+
+    const instanceProfile = new iam.CfnInstanceProfile(this, myProfileName, {
+      instanceProfileName: myProfileName,
+      roles: [
+        myRole.roleName
+      ]
+    });
+
+  // Example of some options which can be configured
+  const optionSettingProperties: elasticbeanstalk.CfnEnvironment.OptionSettingProperty[] = [
+    {
+        namespace: 'aws:autoscaling:launchconfiguration',
+        optionName: 'IamInstanceProfile',
+        value: myProfileName,
+    },
+    {
+        namespace: 'aws:autoscaling:asg',
+        optionName: 'MinSize',
+        value: props?.maxSize ?? '1',
+    },
+    {
+        namespace: 'aws:autoscaling:asg',
+        optionName: 'MaxSize',
+        value: props?.maxSize ?? '1',
+    },
+    {
+        namespace: 'aws:ec2:instances',
+        optionName: 'InstanceTypes',
+        value: props?.instanceTypes ?? 't2.micro',
+    },
+  ];
+
 
   }
 }
